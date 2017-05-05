e56e8aff785b2a8edda8e03b4bd5901b70226e26
MOe_13 <MOetainment13@gmail.com>
2017-05-04 20:48:50 -0400

    my firt app on heroku

--- server.js
+++ server.js
@@ -6,11 +6,18 @@ var app = express();
 
 app.get("/", function(request, respond){
 	//repsond.send("You popping baby");
+	if (process.env.NODE_ENV == "dev"){ 
 respond.json({
 	"user": process.env.DB_USER,
 	"pass": process.env.DB_PASS
 
 });
+}else{
+	respond.json({
+		"user":'Does not exist'
+		"pass":'User does not exist'
+	});
+}
 });
 
 app.listen(8080);
